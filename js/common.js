0081e8ada56f1a3368241f7f60539b01cd59efae
gumi99 <yewongumi@gmail.com>
2024-05-01 14:23:38 +0900

    최종

---
+++ js/common.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){//시작
+
+  $(".logo-wrap .ham-wrap").click(function(){
+    $(this).toggleClass('active');
+    $("nav").slideToggle('show');
+  });
+
+  gsap.registerPlugin(ScrollTrigger) // 스크롤 트리거 등록
+
+  const ani6 = gsap.timeline();
+  ani6.to(".main-txt", {scale: 50, duration: 3, autoAlpha: 1,})
+      .to(".main-txt", {autoAlpha: 0,})
+  
+  ScrollTrigger.create({
+      animation: ani6,
+      trigger: "#main-visual",
+      start: "top top",
+      end: "+=2000",
+      scrub: 1,
+      pin: true,
+      anticipatePin: 1,
+      markers: true
+  });
+});//끝
+
+$(document).ready(function(){//시작
+
+  gsap.registerPlugin(ScrollTrigger); //news 스크롤트리거 시작
+
+  ScrollTrigger.matchMedia({
+    "(min-width: 768px)": function(){
+
+      let list = gsap.utils.toArray('.work ul li');
+      let scrollTween = gsap.to(list,{
+        xPercent: -100 * (list.length - 1),
+        ease: 'none',
+        scrollTrigger: {
+          trigger: '.work',
+          pin : true,
+          pinSpacing:false,
+          scrub: 1,
+          strat: 'center center',
+          end: '+=1000 top', 
+          markers: true
+        }
+      })
+    }
+  })
+
+  var swiper = new Swiper(".product-list", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      768: { //768px 이상
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      1200: {//1200 
+        slidesPerView : 3,
+        spaceBetween: 30
+      }
+    }
+  });
+});//끝
+
+$(document).ready(function(){
+
+  $(".mgnb > li").click(function(){
+
+    if($(window).width() < 767){
+      if($(this).children(".depth2").css("display")=="block"){
+      $(this).children(".depth2").slideUp();
+      $(this).children(".arr").find("span").stop().removeClass("move");
+    }else{
+      $(".depth2").stop().slideUp();
+      $(this).children(".depth2").slideDown();
+      $(this).children(".arr").find("span").stop().addClass("move");
+    };
+    }
+    
+  });
+});
+  
